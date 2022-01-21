0dcfb8f9d4baa48a588b9a2981bd79ecee0f1d9c
litianyi <litianyi@wxchina.com>
2022-01-21 15:41:11 +0800

    添加链表实现

--- src/main/java/com/d9cloud/algorithm/array/ArrayList.java
+++ src/main/java/com/d9cloud/algorithm/array/ArrayList.java
@@ -52,4 +52,15 @@ public class ArrayList<T> {
                 '}';
     }
 
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(3);
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        System.out.println(arrayList);
+        System.out.println(arrayList.get(3));
+        System.out.println(arrayList.size());
+    }
+
 }
--- src/main/java/com/d9cloud/algorithm/link/LinkedList.java
+++ src/main/java/com/d9cloud/algorithm/link/LinkedList.java
@@ -20,6 +20,7 @@ public class LinkedList {
     }
 
     public boolean add(int data) {
+        size++;
         Node node = new Node();
         node.data = data;
         if (head == null) {
@@ -38,18 +39,55 @@ public class LinkedList {
         }
         Node prev = null;
         Node cur = head;
-        while (cur.next != null && cur.data != data) {
+        while (cur != null && cur.data != data) {
             prev = cur;
             cur = cur.next;
         }
-        if (cur == head) {
-            head = cur.next;
-            cur.next = null;
-        }
-        if (cur.data != data) {
+        if (cur == null) {
             return false;
         }
+        if (prev == null) {
+            prev = head;
+            head = head.next;
+            prev.next = null;
+        } else {
+            prev.next = prev.next.next;
+        }
+        size--;
+        return true;
+    }
 
+    public Integer get(int index) {
+        if (size == 0 || index >= size) {
+            throw new RuntimeException("超出范围！");
+        }
+        int curIndex = 0;
+        Node cur = head;
+        while (cur != null) {
+            if (curIndex == index) {
+                break;
+            }
+            curIndex++;
+            cur = cur.next;
+        }
+        if (cur == null) {
+            return null;
+        } else {
+            return cur.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(5);
+        linkedList.add(4);
+        linkedList.add(3);
+        System.out.println("size = " + linkedList.size);
+        System.out.println("index 2: " + linkedList.get(2));
+        linkedList.remove(4);
+        for (int i = 0; i < linkedList.size; i++) {
+            System.out.println("index i: " + linkedList.get(i));
+        }
     }
 
 }
