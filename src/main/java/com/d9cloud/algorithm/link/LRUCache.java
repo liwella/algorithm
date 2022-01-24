659070b09d4c3c1a28e80af841272bca7c08ad32
litianyi <574399358@qq.com>
2022-01-24 23:12:31 +0800

    lru

---
+++ src/main/java/com/d9cloud/algorithm/link/LRUCache.java
@@ -0,0 +1,17 @@
+package com.d9cloud.algorithm.link;
+
+import java.util.LinkedList;
+
+/**
+ * Description:
+ *
+ * @author 57439
+ * @date Created on 2022/1/24
+ */
+public class LRUCache {
+
+    private LinkedList list;
+
+
+
+}
