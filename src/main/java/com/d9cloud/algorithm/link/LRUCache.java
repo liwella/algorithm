a51bde1f9c0c4062e93337bde7a561e61a9e8db8
litianyi <litianyi@wxchina.com>
2022-01-25 13:38:05 +0800

    LRUCache实现

--- src/main/java/com/d9cloud/algorithm/link/LRUCache.java
+++ src/main/java/com/d9cloud/algorithm/link/LRUCache.java
@@ -8,10 +8,54 @@ import java.util.LinkedList;
  * @author 57439
  * @date Created on 2022/1/24
  */
-public class LRUCache {
+public class LRUCache<T> {
 
-    private LinkedList list;
+    private LinkedList<T> list = new LinkedList<>();
 
+    private int capacity = 10;
 
+    public LRUCache() {
+    }
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean get(T t) {
+        T temp = null;
+        for (T item : list) {
+            if (item == t) {
+                temp = item;
+            }
+        }
+        if (temp != null) {
+            list.remove(temp);
+            list.addFirst(t);
+        } else {
+            if (list.size() < capacity) {
+                list.addFirst(t);
+            } else {
+                list.removeLast();
+                list.addFirst(t);
+            }
+        }
+        return true;
+    }
+
+    public void print() {
+        for (T t : list) {
+            System.out.println(t);
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache<Integer> lru = new LRUCache<>();
+        for (int i = 0; i < 10; i++) {
+            lru.get(i);
+        }
+        lru.get(15);
+        lru.get(4);
+        lru.print();
+    }
 
 }
