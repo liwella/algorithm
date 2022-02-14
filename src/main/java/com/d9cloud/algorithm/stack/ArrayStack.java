6440e2e608c7e4a7db3dfa774bd486f57978400e
litianyi <litianyi@wxchina.com>
2022-02-14 17:23:49 +0800

    顺序栈框架

---
+++ src/main/java/com/d9cloud/algorithm/stack/ArrayStack.java
@@ -0,0 +1,35 @@
+package com.d9cloud.algorithm.stack;
+
+/**
+ * Description:
+ *
+ * @author: litianyi
+ * @date: Created on 2022/02/14
+ */
+public class ArrayStack {
+
+    private Integer[] array;
+    private int size;
+    private int capacity;
+
+    public ArrayStack() {
+        this.size = 0;
+        this.capacity = 10;
+        this.array = new Integer[capacity];
+    }
+
+    public void add(Integer number) {
+        if (size == capacity) {
+
+        }
+    }
+
+    public Integer poll() {
+        return null;
+    }
+
+    public Integer peek() {
+        return null;
+    }
+
+}
