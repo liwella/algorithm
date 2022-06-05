6fd9afcc39bbdaac226839466f4a3b8d25c938cb
litianyi <574399358@qq.com>
2022-06-05 21:51:00 +0800

    翻转二叉树

---
+++ src/main/java/com/d9cloud/algorithm/aleetcode/leetcode/editor/cn/InvertBinaryTree.java
@@ -0,0 +1,89 @@
+//给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [2,1,3]
+//输出：[2,3,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 1309 👎 0
+
+package com.d9cloud.algorithm.aleetcode.leetcode.editor.cn;
+
+import com.d9cloud.algorithm.aleetcode.leetcode.editor.cn.entity.TreeNode;
+
+public class InvertBinaryTree {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        deepFirst(root);
+        return root;
+    }
+
+    // 不能使用中序遍历，会有问题
+    private void deepFirst(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        deepFirst(node.left);
+        deepFirst(node.right);
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+    public static void main(String[] args) {
+        Solution solution = new InvertBinaryTree().new Solution();
+    }
+
+}
